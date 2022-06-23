c4916c952d34208ce403fce99a2fe2abda900323
OsasereIk <edenosas1@gmail.com>
2022-06-23 15:38:09 +0100

    still learning

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,6 +2,14 @@
   <div>Hello {{ name }}</div>
   <div v-html="channel"></div>
   <div v-html="hack"></div>
+  <h2 v-bind:id="headingId">Header</h2>
+  <button v-bind:disabled="isDisabled">bind</button>
+  <h2 class="underline">underline text</h2>
+  <h2 class="underline" :class="status">status</h2>
+  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout Movie</h2>
+  <h2 :class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new ']">Array conditional movie</h2>
 </template>
 
 <script>
@@ -13,6 +21,11 @@ export default {
       name: 'Vishas',
       channel: `<b>Codevolution</b>`,
       hack: `<a href="#" onclick="alert('you have been hacked!')">win a prize!</a>`,
+      headingId: 'heading',
+      isDisabled: true,
+      status: 'success',
+      isPromoted: true,
+      isSoldout: true,
     };
   },
 };
@@ -26,5 +39,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: green;
+}
+
+.sold-out {
+  color: crimson;
 }
 </style>
